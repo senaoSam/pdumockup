102e8d6edf0d81fcc7f261e44785bae4a6247359
莊逸軒 <sam.chuang@senao.com>
2022-09-30 14:49:43 +0800

    q

---
+++ precache-manifest.bc08cf2329852d9ae2c24fedeb5f6cae.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529b512fb1f297e673ba974aa06bbd9d",
+    "url": "/pdumockup/index.html"
+  },
+  {
+    "revision": "3e5b23d5b2418ae92a14",
+    "url": "/pdumockup/static/css/2.ac56cf77.chunk.css"
+  },
+  {
+    "revision": "f078bdfb4fe3ac6d056c",
+    "url": "/pdumockup/static/css/main.c66c94e1.chunk.css"
+  },
+  {
+    "revision": "3e5b23d5b2418ae92a14",
+    "url": "/pdumockup/static/js/2.3cb9b693.chunk.js"
+  },
+  {
+    "revision": "ee06d740c1a3de9bae2af303199775c9",
+    "url": "/pdumockup/static/js/2.3cb9b693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f078bdfb4fe3ac6d056c",
+    "url": "/pdumockup/static/js/main.435723a0.chunk.js"
+  },
+  {
+    "revision": "47e9f6732a3fcad17e5b",
+    "url": "/pdumockup/static/js/runtime-main.390ddfa3.js"
+  },
+  {
+    "revision": "b61c8dc792d015dacee2a2969f984795",
+    "url": "/pdumockup/static/media/adaptor.b61c8dc7.svg"
+  },
+  {
+    "revision": "338be757bdb57c2c03d733ec0dcd7293",
+    "url": "/pdumockup/static/media/analyze.338be757.svg"
+  },
+  {
+    "revision": "60d891473e173031cb0d074ad161bf4f",
+    "url": "/pdumockup/static/media/angle-up.60d89147.svg"
+  },
+  {
+    "revision": "23e96553db8d15cddeacc9bed9eea82a",
+    "url": "/pdumockup/static/media/ap.23e96553.svg"
+  },
+  {
+    "revision": "1a73cb75b98dd822851867a73ca6260d",
+    "url": "/pdumockup/static/media/brandLogo.1a73cb75.svg"
+  },
+  {
+    "revision": "0114bb7bbb759e42d64b21fbc3ce6441",
+    "url": "/pdumockup/static/media/circlereset.0114bb7b.svg"
+  },
+  {
+    "revision": "e1928c567e44325a7275a9d78a95f987",
+    "url": "/pdumockup/static/media/diagSwitch.e1928c56.svg"
+  },
+  {
+    "revision": "f0a0e582ccb2b4ee5c8163c61e16358a",
+    "url": "/pdumockup/static/media/errorboundary.f0a0e582.svg"
+  },
+  {
+    "revision": "5c3c6f1f3ec9bdf559d98942d6c09a8a",
+    "url": "/pdumockup/static/media/monitor.5c3c6f1f.svg"
+  },
+  {
+    "revision": "5e6fb68d10b103a935542c66ed7c73b4",
+    "url": "/pdumockup/static/media/phone.5e6fb68d.svg"
+  },
+  {
+    "revision": "397703c13895d94d853684e49f506256",
+    "url": "/pdumockup/static/media/printer.397703c1.svg"
+  },
+  {
+    "revision": "4e1337808ec584ba06e1852cd6a6a802",
+    "url": "/pdumockup/static/media/reset.4e133780.svg"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/pdumockup/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/pdumockup/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/pdumockup/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/pdumockup/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/pdumockup/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/pdumockup/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/pdumockup/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/pdumockup/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/pdumockup/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/pdumockup/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/pdumockup/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/pdumockup/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/pdumockup/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/pdumockup/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/pdumockup/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/pdumockup/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/pdumockup/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/pdumockup/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/pdumockup/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/pdumockup/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/pdumockup/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/pdumockup/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/pdumockup/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/pdumockup/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "87781f067bb09f90179a95c2e869124f",
+    "url": "/pdumockup/static/media/router.87781f06.svg"
+  }
+]);
